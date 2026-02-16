1a074ef998dba280fd2e27100a0d3c4aa704fcd2
Warenn20 <107431282+ABIDOGUNRetis18@users.noreply.github.com>
2026-02-17 00:07:23 +0100

    Add build output directory to Vite config
    Specify output directory for Vercel deployment.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // <-- Vercel cherchera ce dossier après le build
+  }
 })
